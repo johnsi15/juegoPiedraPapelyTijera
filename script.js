05b45d7d0913773399ac42c349e43d771da1c36d
John Andrey <andrey_201015@hotmail.com>
2014-08-14 12:18:53 -0500

    Mejoras
    En el codigo html y en el javaScript

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
-
 //var opciones = ["piedra", "papel", "tijera"];
 var opciones = [0, 1, 2];
 var eleccionMaquina;
 
-function maquina(){
-    eleccionMaquina = Math.floor(Math.random() * 5);
+function aleatorio(minimo, maximo){
+    var numero = Math.floor(Math.random() * (maximo - minimo +1) + minimo);
+    return numero;
 }
 
 function usuario(eleccionUsuario){
- eleccionUsuario = parseInt(eleccionUsuario);
-maquina();
+    eleccionUsuario = parseInt(eleccionUsuario);
+    eleccionMaquina = aleatorio(0,2);
+    alert(eleccionUsuario);
+    alert(eleccionMaquina);
 
-if(eleccionUsuario == 0){
-    if(opciones[eleccionMaquina] == 1){
-        document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio papel y tu piedra.</h3>';
+    if(eleccionUsuario == 0){//el usuario eligio piedra 
+        if(opciones[eleccionMaquina] == 1){//si la maquina eligio papel 
+            document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio papel y tu piedra.</h3>';
+        }else{
+            if(opciones[eleccionMaquina] == 2){
+                document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio tijera y tu piedra.</h3>';
+            }else{
+                if(opciones[eleccionMaquina] == 0){
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron piedra.</h3>';
+                }
+            }
+        } 
     }
-    if(opciones[eleccionMaquina] == 2){
-        document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio tijera y tu piedra.</h3>';
-    }
-    if(opciones[eleccionMaquina] == 0){
-        document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron piedra.</h3>';
-    }
-}
 
-if(eleccionUsuario == 1){
-    if(opciones[eleccionMaquina] == 2){
-        document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio tijera y tu papel.</h3>';
+    if(eleccionUsuario == 1){//el usuario eligio papel 
+        if(opciones[eleccionMaquina] == 2){
+            document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio tijera y tu papel.</h3>';
+        }else{
+            if(opciones[eleccionMaquina] == 0){
+                document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio piedra y tu papel.</h3>';
+                
+            }else{
+                if(opciones[eleccionMaquina] == 1){
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron papel.</h3>'; 
+                }
+            }
+        }
     }
-    if(opciones[eleccionMaquina] == 0){
-        document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio piedra y tu papel.</h3>';
+
+    if(eleccionUsuario == 2) {//el usuario eligio tijera 
+        if(opciones[eleccionMaquina] == 1){
+            document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio papel y tu tijera.</h3>';
         
+        }else{
+            if(opciones[eleccionMaquina] == 0){
+                document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio piedra y tu tijera.</h3>'; 
+            }else{
+                if(opciones[eleccionMaquina] == 2) {
+                    document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron tijera.</h3>';
+                }
+            }
+        }
     }
-    if(opciones[eleccionMaquina] == 1){
-        document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron papel.</h3>';
-        
-    }
-}
 
-if(eleccionUsuario == 2) {
-    if(opciones[eleccionMaquina] == 1){
-        document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio papel y tu tijera.</h3>';
-    
-    }
-    if(opciones[eleccionMaquina] == 0){
-        document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio piedra y tu tijera.</h3>';
-       
-    }
-    if(opciones[eleccionMaquina] == 2) {
-        document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron tijera.</h3>';
-    }
-}
-
-document.getElementById('efecto').style.display = "";
+    document.getElementById('efecto').style.display = "";
 }
 
 
